35d33ac6612109fde91be35f9f170680eb9f3a11
Alexandre De Dommelin <adedommelin@tuxz.net>
2014-02-03 21:16:54 +0100

    Add setup of yum rundeck repo when running on rhel derivative/family

--- manifests/repository.pp
+++ manifests/repository.pp
@@ -8,8 +8,17 @@ class rundeck::repository {
           repository => '/',
         }
       }
+      /(?i:CentOS|Amazon|RedHat)/ : {
+        file { '/etc/yum.repos.d/rundeck.repo':
+          ensure  => 'present',
+          source  => 'puppet:///modules/rundeck/yum/rundeck.repo',
+          owner   => 'root',
+          group   => 'root',
+          mode    => '0644',
+        }
+      }
       default: {
-        notice("Unknown operating system $::operatingsystem")
+        notice("Unknown operating system ${::operatingsystem}")
       }
     }
   }
